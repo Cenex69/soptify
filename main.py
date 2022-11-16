24c301c583de85298372b0819674bb9501d83aef
Cenex69 <100140394+Cenex69@users.noreply.github.com>
2022-11-16 22:18:58 +0530

    main.py
    latest release v0.1 
    (small update: optimised the program, might run faster now)

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ startedCount = False
 isPaused = False
 isActive = False
 count=0
-print("booting 'app'")
 app = Tk()
 app.title("Soptify")
 app.geometry("400x380")
@@ -15,7 +14,6 @@ app.config(bg="black")
 photo1 = ImageTk.PhotoImage(Image.open("play.png").resize((60, 60), Image.ANTIALIAS))
 photo2 = ImageTk.PhotoImage(Image.open("pause.png").resize((60, 60), Image.ANTIALIAS))
 photo3= ImageTk.PhotoImage(Image.open("Spotify.png").resize((300, 100), Image.ANTIALIAS))
-print('tkinter boot finished')
 directory = askdirectory()
 os.chdir(directory)
 songList = os.listdir()
@@ -24,7 +22,6 @@ for item in songList:
     pos = 0
     playList.insert(pos, item)
     pos += 1
-print("booting pygame.mixer")
 pyg.init()
 pyg.mixer.init(frequency=44100, size=-16, channels=2, buffer=512, devicename=None)
 
@@ -39,19 +36,18 @@ def pause():
     if isActive is not False:
         isPaused = True
         pyg.mixer.music.pause()
-        print("paused")
     else:
         isPaused = False
-        print("unable to pause")
+        print("ERROR: unable to pause")
 def _start():
     global count, startedCount
     if startedCount is True:
-        print("startedCount = True")
+        print("sc=true")
         count += 1
         print(count)
         return convert(count)
     else:
-        print("startedCount = False")
+        print("sc=false")
     app.after(ms=1000, func=_start)
 
 def play():
@@ -61,21 +57,16 @@ def play():
         songTitle.config(font="Calibri 32")
         app.geometry("400x380")
         song = playList.get(ACTIVE)
-        print(f"loading {song}")
         if len(str(song)) > 35:
             songTitle.config(font="Calibri 13")
             app.geometry("400x360")
-            print("ERROR: length above limit\nALTERNATIVE: setting geometry to 400x262")
         elif len(str(song)) > 24:
             songTitle.config(font="Calibri 28")
             app.geometry("400x380")
-            print("ERROR: length above limit\nALTERNATIVE: setting geometry to 400x262")
         pyg.mixer.music.load(song)
-        print(f"loaded {song}")
         l.set(f"Around {convert(int(MP3(song).info.length))} minutes long.")
         var.set(str(playList.get(ACTIVE)).split(".")[0])
         pyg.mixer.music.play(0)
-        print(f"playing {song}")
     elif isPaused is True:
         if playList.get(ACTIVE) != song:
             isActive = True
@@ -86,21 +77,16 @@ def play():
             if len(str(song)) > 35:
                 songTitle.config(font="Calibri 13")
                 app.geometry("400x360")
-                print("ERROR: length above limit\nALTERNATIVE: setting geometry to 400x262")
             elif len(str(song)) > 24:
                 songTitle.config(font="Calibri 28")
                 app.geometry("400x380")
-                print("ERROR: length above limit\nALTERNATIVE: setting geometry to 400x262")
             pyg.mixer.music.load(song)
-            print(f"loaded {song}")
             l.set(f"Around {convert(int(MP3(song).info.length))} minutes long.")
             var.set(str(playList.get(ACTIVE)).split(".")[0])
             pyg.mixer.music.play(0)
-            print(f"playing {song}")
         else:
             pyg.mixer.music.unpause()
             isPaused = False
-            print("resumed")
 Play = Button(app, image=photo1,command=play, border=0, borderwidth=0, width=56, height=56)
 Pause = Button(app, image=photo2,command=pause, border=0, borderwidth=0, width=56, height=56)
 var = StringVar()
